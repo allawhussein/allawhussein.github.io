ed069ee8eac88a9d849f22daa81fb098d4f8a10d
allawhussein <allawhussein@gmail.com>
2022-05-03 20:54:45 +0300

    added audio pause/play methods

--- audio-script.js
+++ audio-script.js
@@ -32,12 +32,14 @@ function playSong(){
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
 }
 
 function pauseSong(){
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
     playBtn.querySelector('i.fas').classList.add('fa-play');
+    audio.pause();
 }
 
 //Event Listeners
