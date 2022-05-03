1ec4605fa1077aa7960fe395aae98ef736a677d9
allawhussein <allawhussein@gmail.com>
2022-05-03 20:29:10 +0300

    added comments, added loadSong(song), songs[], ...

--- audio-script.js
+++ audio-script.js
@@ -1,3 +1,4 @@
+//Bunch of Definitions for DOM selection
 const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
@@ -10,3 +11,19 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 const currTime = document.querySelector('#currTime');
 const durTime = document.querySelector('#durTime');
+
+//List of songs, should better scan the songs folder for aviable ones, and then create such a list
+const songs = ['أخبرني', 'يه جا براى من تو اين حرم است', 'امشب در سر', 'عبق الأمل'];
+
+//Keep track of the current song
+let songIndex = 2;
+
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song){
+    //We need to update the title, actual song, image
+    title.innerText = song;
+    audio.src = 'music-albums/music-tracks/${song}.mp3';
+    cover.src = 'music-albums/music-images/${song}.jpg';
+}
